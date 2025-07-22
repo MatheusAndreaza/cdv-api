fbc0adb13f6aab5881bd8dbbaff129ff49bc882c
Matheus Andreaza <matheus20203040@hotmail.com>
2025-07-22 16:03:20 -0300

    Adicionar métodos salvar e listar na classe PecaService

--- src/main/java/br/com/andreaza/cdv_api/controller/PecaController.java
+++ src/main/java/br/com/andreaza/cdv_api/controller/PecaController.java
@@ -1,4 +1,15 @@
 package br.com.andreaza.cdv_api.controller;
 
+import br.com.andreaza.cdv_api.entity.Peca;
+import br.com.andreaza.cdv_api.service.PecaService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/pecas")
 public class PecaController {
+
+
 }
--- src/main/java/br/com/andreaza/cdv_api/service/PecaService.java
+++ src/main/java/br/com/andreaza/cdv_api/service/PecaService.java
@@ -1,4 +1,25 @@
 package br.com.andreaza.cdv_api.service;
 
+import br.com.andreaza.cdv_api.entity.Peca;
+import br.com.andreaza.cdv_api.repository.PecaRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class PecaService {
+
+    private PecaRepository pecaRepository;
+
+    public PecaService(PecaRepository pecaRepository) {
+        this.pecaRepository = pecaRepository;
+    }
+
+    public Peca salvar(Peca peca) {
+        return pecaRepository.save(peca);
+    }
+
+    public List<Peca> listar() {
+        return pecaRepository.findAll();
+    }
 }
